0bf5d694cedb4368845dfa133ab45f8ecdb8d72f
Mike Pierce <mike@someoneandsons.net>
2013-05-15 18:46:45 +0200

    Updated single loop template

--- parts/loop-single.php
+++ parts/loop-single.php
@@ -1,7 +1,9 @@
 <?php while ( have_posts() ) : the_post(); ?>
 	<article>
-		<h3><?php the_title(); ?></h3>
-		<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+		<h2><?php the_title(); ?></h2>
+		<? if(is_single()): ?>
+			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+		<? endif; ?>
 		<?php the_content(); ?>
 	</article>
 <?php endwhile; ?>
